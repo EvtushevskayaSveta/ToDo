a43df31c31620c4b3a90f039624025fefc02d6b4
EvtushevskayaSveta <evtushevskaya.lana@gmail.com>
2022-07-04 23:07:31 +0300

    add Navbar

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import TodoList from './to_do/TodoList';
+import Navbar from './to_do/Navbar';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>To do list</Text>
+      <Navbar title="Todo app" />
       <TodoList />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 40,
-    windth: 600,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
+  container: {},
 });
 
 export default App;
---
+++ to_do/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+const Navbar = props => {
+  return (
+    <View style={styles.navbar}>
+      <Text style={styles.text}>{props.title}</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  navbar: {
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: '#808080',
+    paddingBottom: 10,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 20,
+    color: 'while',
+  },
+});
+export default Navbar;
